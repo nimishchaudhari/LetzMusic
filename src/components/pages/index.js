0d1393641613976e530cc86a0cf827d47116f097
Nimish Chaudhari <37145066+nimishchaudhari@users.noreply.github.com>
2025-04-02 11:12:03 +0200

    Add index.js for cleaner imports

---
+++ src/components/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from './Home';
+export { default as Dashboard } from './Dashboard';
+export { default as NoteExplorer } from './NoteExplorer';
+export { default as IntervalDemonstrator } from './IntervalDemonstrator';
