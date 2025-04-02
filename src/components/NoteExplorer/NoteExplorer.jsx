d7a46819f930adcd1e484c18b1a1fa223b610a38
Nimish Chaudhari <37145066+nimishchaudhari@users.noreply.github.com>
2025-04-02 11:37:33 +0200

    Create NoteExplorer component scaffold

---
+++ src/components/NoteExplorer/NoteExplorer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NoteExplorer = () => {
+  return (
+    <div>
+      <h2>Note Explorer</h2>
+      {/* TODO: Implement note explorer UI */}
+    </div>
+  );
+};
+
+export default NoteExplorer;
