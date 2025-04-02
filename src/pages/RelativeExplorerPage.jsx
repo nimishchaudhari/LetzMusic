b51bf1b1d41409a3df84d427e04d9fef3adcb637
Nimish Chaudhari <37145066+nimishchaudhari@users.noreply.github.com>
2025-04-02 14:49:52 +0200

    Add RelativeExplorerPage component

---
+++ src/pages/RelativeExplorerPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RelativeExplorer from '../components/RelativeExplorer/RelativeExplorer';
+
+const RelativeExplorerPage = () => {
+  return (
+    <div className="container">
+      <RelativeExplorer />
+    </div>
+  );
+};
+
+export default RelativeExplorerPage;
