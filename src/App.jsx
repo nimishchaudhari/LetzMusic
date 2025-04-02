9f779a6c3494f4b155fdf1356a40c7a15801dec1
Nimish Chaudhari <37145066+nimishchaudhari@users.noreply.github.com>
2025-04-02 11:46:52 +0200

    Add route for NoteExplorerPage in main App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { 
-  Home,
-  Dashboard,
-  NoteExplorer,
-  IntervalDemonstrator 
-} from './components/pages';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from './contexts/ThemeContext';
+import { ProgressionProvider } from './contexts/ProgressionContext';
+import { PlaybackProvider } from './contexts/PlaybackContext';
+import Home from './pages/Home/Home';
+import Dashboard from './pages/Dashboard/Dashboard';
+import NoteExplorerPage from './pages/NoteExplorerPage/NoteExplorerPage';
 
 const App = () => {
   return (
-    <div className="app">
-      <main className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/note-explorer" element={<NoteExplorer />} />
-          <Route path="/interval-demonstrator" element={<IntervalDemonstrator />} />
-        </Routes>
-      </main>
-    </div>
+    <ThemeProvider>
+      <ProgressionProvider>
+        <PlaybackProvider>  
+          <Router>
+            <div className="app">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/notes" element={<NoteExplorerPage />} />
+              </Routes>
+            </div>
+          </Router>
+        </PlaybackProvider>
+      </ProgressionProvider>  
+    </ThemeProvider>
   );
 };
 
