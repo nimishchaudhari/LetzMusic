05ec4f9c18b3475bcb61b15bafd4b9bb1585940d
Nimish Chaudhari <37145066+nimishchaudhari@users.noreply.github.com>
2025-04-02 14:36:06 +0200

    Add IntervalDemonstratorPage component

---
+++ src/pages/IntervalDemonstratorPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import IntervalDemonstrator from '../components/IntervalDemonstrator/IntervalDemonstrator';
+
+const IntervalDemonstratorPage = () => {
+  return (
+    <div className="container">
+      <IntervalDemonstrator />
+    </div>
+  );
+};
+
+export default IntervalDemonstratorPage;
