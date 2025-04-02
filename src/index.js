a3d4afc98ee3a9a5796a9cc13b3729fad3e37d80
Nimish Chaudhari <37145066+nimishchaudhari@users.noreply.github.com>
2025-04-02 10:51:31 +0200

    Update Router to HashRouter for GitHub Pages compatibility and add missing providers

--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import UserProgressProvider from './contexts/UserProgressContext';
+import AudioProvider from './contexts/AudioContext';
+import PreferencesProvider from './contexts/PreferencesContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
-      <UserProgressProvider>
-        <App />
-      </UserProgressProvider>
+    <Router>
+      <AudioProvider>
+        <UserProgressProvider>
+          <PreferencesProvider>
+            <App />
+          </PreferencesProvider>
+        </UserProgressProvider>
+      </AudioProvider>
     </Router>
   </React.StrictMode>
 );
