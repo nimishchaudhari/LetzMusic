aae99efb493eeca1419063c2d558233f84e1bd6f
Nimish Chaudhari <37145066+nimishchaudhari@users.noreply.github.com>
2025-04-02 11:38:26 +0200

    Integrate keyboard and display components

--- src/components/NoteExplorer/NoteExplorer.jsx
+++ src/components/NoteExplorer/NoteExplorer.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Keyboard from './Keyboard';
+import NoteDisplay from './NoteDisplay';
 
 const NoteExplorer = () => {
+  const [currentNote, setCurrentNote] = useState(null);
+
+  const handleNoteSelect = (note) => {
+    setCurrentNote(note);
+    // TODO: Trigger note playback
+  };
+
   return (
-    <div>
+    <div className="note-explorer">
       <h2>Note Explorer</h2>
-      {/* TODO: Implement note explorer UI */}
+      <Keyboard onNoteSelect={handleNoteSelect} />
+      <NoteDisplay note={currentNote} />
     </div>
   );
 };
