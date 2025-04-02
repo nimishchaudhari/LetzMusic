daa1bcf35a284d9c16ab24632161aa0a9899798e
Nimish Chaudhari <37145066+nimishchaudhari@users.noreply.github.com>
2025-04-02 16:16:44 +0200

    Update Navigation with links to new components

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navigation.css';
 
+/**
+ * Navigation - Main navigation component with links to all tools
+ */
 const Navigation = () => {
   return (
     <nav className="navigation">
+      <div className="logo">LetzMusic</div>
       <ul>
         <li>
-          <NavLink to="/" exact="true">
+          <NavLink to="/" end>
             Home
           </NavLink>
         </li>
@@ -16,6 +20,15 @@ const Navigation = () => {
         </li>
         <li>
           <NavLink to="/notes">Note Explorer</NavLink>
+        </li>
+        <li>
+          <NavLink to="/circle-of-fifths">Circle of Fifths</NavLink>
+        </li>
+        <li>
+          <NavLink to="/chord-visualizer">Chord Visualizer</NavLink>
+        </li>
+        <li>
+          <NavLink to="/mode-transformer">Mode Transformer</NavLink>
         </li>
       </ul>
     </nav>
