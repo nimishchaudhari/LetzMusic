800c4f07685ae50dc2b37fd8ab37a241e814cd04
Nimish Chaudhari <37145066+nimishchaudhari@users.noreply.github.com>
2025-04-02 14:47:59 +0200

    Add ScaleLabPage component

---
+++ src/pages/ScaleLabPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ScaleLab from '../components/ScaleLab/ScaleLab';
+
+const ScaleLabPage = () => {
+  return (
+    <div className="container">
+      <ScaleLab />
+    </div>
+  );
+};
+
+export default ScaleLabPage;
