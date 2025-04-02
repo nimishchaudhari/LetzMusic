101c045a8e70fddccd3ca4af419f1e2b2e19762d
Nimish Chaudhari <37145066+nimishchaudhari@users.noreply.github.com>
2025-04-02 11:11:46 +0200

    Add IntervalDemonstrator component

---
+++ src/components/pages/IntervalDemonstrator.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IntervalDemonstrator = () => {
+  return (
+    <div className="interval-demonstrator">
+      <h2>Interval Demonstrator</h2>
+      <p>Learn about musical intervals and their relationships.</p>
+    </div>
+  );
+};
+
+export default IntervalDemonstrator;
